eb3b0734600096cb0569d173cd90410787342062
WaiyiC <waiyi101@gmail.com>
2024-01-10 15:18:36 +0800

    Initial Commit

---
+++ propertyKing/propertyKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  propertyKingApp.swift
+//  propertyKing
+//
+//  Created by Waiyi on 10/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct propertyKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
