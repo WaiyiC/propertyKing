b2647ff346fb5e1172dd033aa6b2734e8ce364fc
WaiyiC <waiyi101@gmail.com>
2024-01-18 13:34:02 +0800

    FaceID fixed location

---
+++ propertyKing/Preview Content/upload.swift
@@ -0,0 +1,18 @@
+//
+//  upload.swift
+//  propertyKing
+//
+//  Created by Waiyi on 16/1/2024.
+//
+
+import SwiftUI
+
+struct upload: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    upload()
+}
---
+++ propertyKing/model/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  propertyKing
+//
+//  Created by Waiyi on 16/1/2024.
+//
+
+import Foundation
