b6d61b8dfd74d1f750b60d964131b97161dbe231
WaiyiC <waiyi101@gmail.com>
2024-01-30 00:12:34 +0800

    /

---
+++ propertyKing/addPhoto.swift
@@ -0,0 +1,18 @@
+//
+//  addPhoto.swift
+//  propertyKing
+//
+//  Created by Waiyi on 29/1/2024.
+//
+
+import SwiftUI
+
+struct addPhoto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addPhoto()
+}
